ddfb7c81b23193d7d6aecc8fee20a99bd571c88a
bcht <bch1145@outlook.com>
2026-04-22 23:31:47 +0800

    feat:增加了异步

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from config import *
 from user_json import *
-from openai import OpenAI
-from flask import Flask, request
+from openai import OpenAI,AsyncOpenAI
+from fastapi import *
 from datetime import datetime
+from fastapi.responses import *
 import os
 import json
 import requests
@@ -11,6 +12,10 @@ import logging
 import config
 import importlib
 import ota
+import uvicorn
+import httpx
+import asyncio
+import aiofiles
 
 #首先，去他丫的LOGO
 #我肯定是不会写LOGO，占地
@@ -29,8 +34,11 @@ if not os.path.exists(os.path.join(path,'user_json')):#初始化ing
     logging.warning('数据库不存在，创建')
     os.makedirs(os.path.join(path,'user_json'))
 
-server = Flask('Elaina',template_folder='Elaina',static_folder='Elaina')
+server = FastAPI('Elaina')
 client_version = 'v1.1.0'# 机器人版本，用于OTA，不要修改
+openai_client = AsyncOpenAI(api_key=api_key,base_url=api_address)#构建AI客户端 APIKey或许也就在这里用了吧
+file_lock = asyncio.Lock() #谁持锁，这文件就是谁的天下。函数啊，大文件…就给你了…(趋势)(大清就交给你了)
+user_locks = {}  # 存储每个用户的锁
 
 def get_formatted_time():
     """返回当前时间，格式为 '年份-月份-日期-小时:分钟:秒'"""
@@ -47,7 +55,14 @@ def hot_reload_config():
     并且忽视私有变量(下划线开头)
     """
 
-def send_msg(msg,uid : int,gid : int,mid = None) -> None:
+async def get_user_lock(uid: int):
+    """获取用户专属的异步锁"""
+    async with file_lock:
+        if uid not in user_locks:
+            user_locks[uid] = asyncio.Lock()
+        return user_locks[uid]
+
+async def send_msg(msg,uid : int,gid : int,mid = None) -> None:
     """发送信息"""
     url = server_address
     data = {}
@@ -64,15 +79,16 @@ def send_msg(msg,uid : int,gid : int,mid = None) -> None:
         data.update({'message':f'{msg}'})
     
     try:
-        requests.post(url,json=data,timeout=5)
-    except requests.exceptions.RequestException:
-        pass
+        async with httpx.AsyncClient() as client:#使用异步调用post
+            await client.post(url,json=data,timeout=5)
+        logging.debug('已发送消息')
+        #但愿以后我用不到这行requests
+        #requests.post(url,json=data,timeout=5)
     except:
         logging.exception('send_msg出现错误,呃,我也不知道是啥')
 
 @server.post('/')
-def auto_reply_message():
-    data = request.get_json()
+async def auto_reply_message(data: dict):
     uid = data.get("user_id")#目标qq
     gid = data.get("group_id")#群聊qq
     mid = data.get("message_id")#消息编号
@@ -82,148 +98,150 @@ def auto_reply_message():
         msg = msg.replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", "&").replace("&#44;", ",")#转码
         if uid==2854196310:#这里是防Q群管家
             logging.debug('Q群管家at你了')
-            return "OK",200
+            return {}
         #加载json用户信息
         user = User(uid,path)
 
         if msg == '/help':#用于获取帮助
             try:
                 with open('help.txt','r',encoding='utf-8') as f:#防手欠x2
-                    send_msg(f'{f.read()}',uid,gid)
+                    await send_msg(f'{f.read()}',uid,gid)
             except FileNotFoundError:
-                send_msg('未找到帮助文档文件',uid,gid)
+                await send_msg('未找到帮助文档文件',uid,gid)
                 logging.exception('未找到帮助文档文件，请确认help.txt是否存在且未重命名')
-                return "OK",200
-            return "OK",200
+                return {}
+            return {}
 
         if msg == '/重载' and uid in admin:
             hot_reload_config()
-            send_msg('已重载配置文件',uid,gid)
+            await send_msg('已重载配置文件',uid,gid)
 
         if msg == '/删除聊天记录':
             logging.info(f'{uid}删除了除好感度之外的所有记录')
             #删除处好感度以外的所有数据
-            send_msg('聊天记录已清除，其余未变',uid,gid)
-            user_info = user.load()
+            await send_msg('聊天记录已清除，其余未变',uid,gid)
+            user_info = await user.load()
             user_info['message'] = []
-            user.write(user_info)
-            return "OK",200
+            await user.write(user_info)
+            return {}
         
         if msg == '/重置聊天记录':
             logging.info(f'{uid}删除了所有记录')
             #全清了！！！
-            send_msg('记录已全部删除',uid,gid)
-            user.delete()
-            return "OK",200
+            await send_msg('记录已全部删除',uid,gid)
+            await user.delete()
+            return {}
         
         if msg.startswith('/撤回上一条'):
             logging.info(f'{uid}撤回了上一条')
-            user_info = user.load()
+            user_info = await user.load()
             if user_info['message'] != []:#删两遍，因为同时有机器人和用户的对话，删的是一回合
                 s = user_info['message'].pop()#如果没毛病，那这里就是机器人的回复了(没想到吧我pop()有返回值)
                 user_info['message'].pop()
                 user_info['time'].pop()
                 if user_info['favor'] < 100:#对于防掉好感度，我还真没好的法子，只能检测是不是满的了
                     user_info['favor'] = user_info['favor'] - ast.literal_eval(s['content'])['favor']#减就相当于反向操作了
-                user.write(user_info)
-                send_msg('已撤回，好感度恢复',uid,gid)
+                await user.write(user_info)
+                await send_msg('已撤回，好感度恢复',uid,gid)
             else:
                 logging.info(f'{uid}但他似乎没聊过…')
-                send_msg('你似乎没的可撤回…',uid,gid)
+                await send_msg('你似乎没的可撤回…',uid,gid)
         
         if  msg.startswith('/查看好感度'):
             logging.info(f'{uid}查看了好感度')
-            user_info = user.load()
-            send_msg(f'当前好感度：{user_info["favor"]}',uid,gid)
+            user_info = await user.load()
+            await send_msg(f'当前好感度：{user_info["favor"]}',uid,gid)
         
         if msg.startswith('/查看上一条'):
             logging.info(f'{uid}查看了上一条')
-            user_info = user.load()
+            user_info = await user.load()
             if user_info['message'] != []:
-                send_msg(f'你：{user_info["message"][-2]['content']}\n我：{ast.literal_eval(user_info["message"][-1]['content'])['message']}\n时间：{user_info['time'][-1]}',uid,gid)
+                await send_msg(f'你：{user_info["message"][-2]['content']}\n我：{ast.literal_eval(user_info["message"][-1]['content'])['message']}\n时间：{user_info['time'][-1]}',uid,gid)
             else:
                 logging.info(f'{uid}但似乎没的可回顾…')
-                send_msg('你似乎没的可回顾…',uid,gid)
+                await send_msg('你似乎没的可回顾…',uid,gid)
         
+
         if msg.startswith(f'[CQ:at,qq={bot_qq}]') or msg.startswith('/AI'):#判断是否是AI聊天
             logging.info(f'{uid}触发了AI聊天')
             if msg.startswith(f'[CQ:at,qq={bot_qq}]'):#删除那些七七八八的触发指令
                 msg = msg.replace(f'[CQ:at,qq={bot_qq}]','',1)
             else:
                 msg = msg.replace('/AI','',1)
-            
-            user_info = user.load()#读取用户信息
-            user_info['message'].append({'role':'user','content':msg})#直接把用户的对话加进去吧
-            tsc = f"""好感总值：{user_info.get('favor')}\n好感总值范围:{love_up}~{-love_up}"""#提示词的附加板块
-            logging.debug(f'{uid}提示词：{tsc}')
-            try:
-                logging.debug(f'{uid}读取人设')
-                with open(prompt_md,'r',encoding='utf-8') as f: #读取人设 是的我已经在编码上吃了一堆坑了
-                    tsc = f.read() + tsc
-            except FileNotFoundError:#为了防止有某位人类手欠
-                send_msg('人设文件不存在，请检查文件名和文件存在情况',uid,gid)
-                logging.exception('人设文件不存在，请检查文件名和文件存在情况')
-                return "OK",200
-            message = [{'role':'system','content':tsc}] + user_info.get('message')
+            lock = await get_user_lock(uid)
+            async with lock:#异步锁，防串
+                user_info = await user.load()#读取用户信息
+                user_info['message'].append({'role':'user','content':msg})#直接把用户的对话加进去吧
+                tsc = f"""好感总值：{user_info.get('favor')}\n好感总值范围:{love_up}~{-love_up}"""#提示词的附加板块
+                logging.debug(f'{uid}提示词：{tsc}')
+                try:
+                    logging.debug(f'{uid}读取人设')
+                    async with aiofiles.open(prompt_md,'r',encoding='utf-8') as f: #读取人设 是的我已经在编码上吃了一堆坑了
+                        tsc = await f.read() + tsc
+                except FileNotFoundError:#为了防止有某位人类手欠
+                    await send_msg('人设文件不存在，请检查文件名和文件存在情况',uid,gid)
+                    logging.exception('人设文件不存在，请检查文件名和文件存在情况')
+                    return {}
+                message = [{'role':'system','content':tsc}] + user_info.get('message')
 
 
-            logging.debug(f'{uid}发送了请求')
-            openai_client = OpenAI(api_key=api_key,base_url=api_address)#构建AI客户端 APIKey或许也就在这里用了吧
-            response = openai_client.chat.completions.create(   #发送请求
-                model=api_AI_model,#模型
-                messages=message,#消息
-                temperature=api_temperature,#温度，我个人习惯1.3
-                max_tokens=api_max_tokens,#最大生成tokens，我个人习惯4096
-                frequency_penalty=1,
-                stream=False,
-                response_format={
-                    'type':'json_object'#确保必须是结构化输出
-                }
-            )
-            logging.debug(f'{uid}请求已完成')
-            try:
-                s = json.loads(response.choices[0].message.content)#防止AI突然抽风不给我好的json
-            except json.JSONDecodeError:
-                if force_json:
-                    logging.warning(f'{uid}的AI返回了非结构化数据，尝试强制解析')
-                    try:
-                        s = ast.literal_eval(response.choices[0].message.content)
-                    except Exception:
-                        send_msg('强制解析出现错误，请联系管理员',uid,gid)
-                        logging.exception(f'{uid}强制解析错误')
+                logging.debug(f'{uid}发送了请求')
+
+                response = await openai_client.chat.completions.create(   #发送请求
+                    model=api_AI_model,#模型
+                    messages=message,#消息
+                    temperature=api_temperature,#温度，我个人习惯1.3
+                    max_tokens=api_max_tokens,#最大生成tokens，我个人习惯4096
+                    frequency_penalty=1,
+                    stream=False,
+                    response_format={
+                        'type':'json_object'#确保必须是结构化输出
+                    }
+                )
+                logging.debug(f'{uid}请求已完成')
+                try:
+                    s = json.loads(response.choices[0].message.content)#防止AI突然抽风不给我好的json
+                except json.JSONDecodeError:
+                    if force_json:
+                        logging.warning(f'{uid}的AI返回了非结构化数据，尝试强制解析')
+                        try:
+                            s = ast.literal_eval(response.choices[0].message.content)
+                        except Exception:
+                            await send_msg('强制解析出现错误，请联系管理员',uid,gid)
+                            logging.exception(f'{uid}强制解析错误')
+                            logging.error(response.choices[0].message.content)
+                            return {}
+                    else:
+                        await send_msg('解析出现错误，请联系管理员',uid,gid)
+                        logging.exception(f'{uid}解析出现错误，真发生了?')
                         logging.error(response.choices[0].message.content)
-                        return "OK",200
-                else:
-                    send_msg('解析出现错误，请联系管理员',uid,gid)
+                        return {}
+                except Exception:
+                    await send_msg('解析出现错误，请联系管理员',uid,gid)
+                    #这种情况很少发生，不过为了输出的美观，还是不要在没结构化的情况下输出吧
+                    #不过懂点的朋友可以自行修改，毕竟这是我个人的喜好
                     logging.exception(f'{uid}解析出现错误，真发生了?')
                     logging.error(response.choices[0].message.content)
-                    return "OK",200
-            except Exception:
-                send_msg('解析出现错误，请联系管理员',uid,gid)
-                #这种情况很少发生，不过为了输出的美观，还是不要在没结构化的情况下输出吧
-                #不过懂点的朋友可以自行修改，毕竟这是我个人的喜好
-                logging.exception(f'{uid}解析出现错误，真发生了?')
-                logging.error(response.choices[0].message.content)
-                return "OK",200
-            
-            send_msg(f'[CQ:at,qq={uid}] '+s.get('message'),uid,gid)#发送并at
+                    return {}
 
-            logging.debug(f'{uid}后处理')
-            user_info['message'].append({'role':'assistant','content':str(s)})#录入
-            user_info['time'].append(get_formatted_time())
-            user_info['favor'] += s.get('favor')
-            if len(user_info.get('message')) > message_up*2:#防超限，实际上它的限制对话是当前这个数字除二  #卧槽我差点忘了还要写0（到时候全删了是吧？？？）
-                del user_info['message'][0]#删最早用户对话
-                del user_info['message'][0]#删最早机器人对话
-                del user_info['time'][0]#删最早时间记录
-            if user_info.get('favor') > love_up:
-                user_info['favor'] = love_up
-            elif user_info.get('favor') < -love_up:
-                user_info['favor'] = -love_up
+                await send_msg(f'[CQ:at,qq={uid}] '+s.get('message'),uid,gid)#发送并at
 
-            user.write(user_info)#写入
-            #如果这还报错你可以骂我了
-    return "OK",200
+                logging.debug(f'{uid}后处理')
+                user_info['message'].append({'role':'assistant','content':str(s)})#录入
+                user_info['time'].append(get_formatted_time())
+                user_info['favor'] += s.get('favor')
+                if len(user_info.get('message')) > message_up*2:#防超限，实际上它的限制对话是当前这个数字除二  #卧槽我差点忘了还要写0（到时候全删了是吧？？？）
+                    del user_info['message'][0]#删最早用户对话
+                    del user_info['message'][0]#删最早机器人对话
+                    del user_info['time'][0]#删最早时间记录
+                if user_info.get('favor') > love_up:
+                    user_info['favor'] = love_up
+                elif user_info.get('favor') < -love_up:
+                    user_info['favor'] = -love_up
+
+                await user.write(user_info)#写入
+                #如果这还报错你可以骂我了
+    return {}
 
 if __name__ == '__main__':#但愿没人闲的没事把这玩意当模块跑
     logging.info(f'当前版本:{client_version}')
@@ -232,4 +250,4 @@ if __name__ == '__main__':#但愿没人闲的没事把这玩意当模块跑
         success, msg = ota.ota_update(client_version, github_repo, auto_restart=True)
         logging.info(msg)
         
-    server.run(host=client_address,port=client_port,debug=False)#每日禁用debug(1/1)
+    uvicorn.run(server,host=client_address,port=client_port)#每日禁用debug(1/1)
--- user_json.py
+++ user_json.py
@@ -1,6 +1,8 @@
 import json
 import os
 import logging
+import asyncio
+import aiofiles
 #主要使用json文件进行处理
 #MySQL?在编了在编了
 
@@ -14,17 +16,12 @@ user_template = {#用户文件模版
 
 def is_user_template_complete(data : dict ) -> bool:#用于检查传入的字典是否符合规范
     """
-    输入
-
-    data->dict
-
-    输出
-
-    bool
-
-    用于检查用户信息是否符合模版，返回True表示符合模版，返回False表示不符合模版
-
-    防呆不防傻，只要我不傻，并且用这个，估计就没毛病
+    输入  
+    data->dict  
+    输出  
+    bool  
+    用于检查用户信息是否符合模版，返回True表示符合模版，返回False表示不符合模版  
+    防呆不防傻，只要我不傻，并且用这个，估计就没毛病  
     """
     # 必需键集合
     required_keys = {"message", "favor", "time"}
@@ -47,38 +44,36 @@ class User:
         self.uid = uid
         self.path = os.path.join(path,'user_json',str(self.uid)+'.json')
     
-    def load(self) -> dict:
+    async def load(self) -> dict:
         """若存在用户文件，则返回用户的信息
         
         若不存在，则返回初始模版并创建"""
         user = {}
         if os.path.isfile(self.path):
-            with open(self.path,'r',encoding='utf-8') as f:
-                user = json.load(f)
+            async with aiofiles.open(self.path,'r',encoding='utf-8') as f:
+                user = json.loads(await f.read())
         else:
-            with open(self.path,'w',encoding='utf-8') as f:
-                json.dump(user_template,f,ensure_ascii=False,indent=4)
+            async with aiofiles.open(self.path,'w',encoding='utf-8') as f:
+                await f.write(json.dumps(user_template,ensure_ascii=False,indent=4))
                 user = user_template
         return user
     
-    def delete(self) -> None:
+    async def delete(self) -> None:
         """重置用户文件，什么都不返回"""
-        with open(self.path,'w',encoding='utf-8') as f:
-            json.dump(user_template,f,ensure_ascii=False,indent=4)
+        async with aiofiles.open(self.path,'w',encoding='utf-8') as f:
+            await f.write(json.dumps(user_template,ensure_ascii=False,indent=4))
 
-    def write(self,data) -> None:
-        """将用户信息写入文件
-        
-        注意，别传入一个不是字典的玩意
-        
+    async def write(self,data) -> None:
+        """将用户信息写入文件  
+        注意，别传入一个不是字典的玩意  
         不要让我在修bug的时候看到这玩意报错!!!"""
         if not is_user_template_complete(data):#我不管，就算我提醒了我也要做个防备措施
             logging.error(f'{self.uid}模版不匹配！')
             raise ValueError('模版不匹配！李在干什麽？')
             return #我知道这行没意义，但是我就是习惯了
         
-        with open(self.path,'w',encoding='utf-8') as f:
-            json.dump(data,f,ensure_ascii=False,indent=4)
+        async with aiofiles.open(self.path,'w',encoding='utf-8') as f:
+            await f.write(json.dumps(data,ensure_ascii=False,indent=4))
     
 if __name__ == '__main__':
     print('我还是很好奇，你运行这个干啥')
